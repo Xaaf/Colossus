5bbdf7c82ddf47d68758caf5c1f144a097cb457f
Xaaf <x.vandalen@outlook.com>
2024-06-12 23:19:36 +0200

    Added a very hacky triangle to test rendering

--- engine/src/Colossus/Render/Renderers/OpenGLRenderer.cpp
+++ engine/src/Colossus/Render/Renderers/OpenGLRenderer.cpp
@@ -4,12 +4,112 @@
 
 using namespace Colossus;
 
+float verts[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+unsigned int inds[] = {0, 1, 2};
+
+const char* vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char* fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+    "}\n\0";
+
+unsigned int vertexShader = 0;
+unsigned int fragmentShader = 0;
+
+unsigned int shaderProgram = 0;
+
+unsigned int VBO, VAO, EBO = 0;
+
+void temp_shaders() {
+    //==== Build Shader Program
+    // Vertex Shader
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // Check for compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        LOG_WARN("OpenGL", "Failed to compile vertex shader\n" << infoLog);
+    }
+    LOG_INFO("OpenGL", "Compiled shader...");
+
+    // Fragment Shader
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Check for compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        LOG_WARN("OpenGL", "Failed to compile fragment shader\n" << infoLog);
+    }
+    LOG_INFO("OpenGL", "Compiled shader...");
+
+    //==== Link Shader Program
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        LOG_WARN("OpenGL", "Failed to link shader program\n" << infoLog);
+    }
+    LOG_INFO("OpenGL", "Linked shader...");
+
+    // Cleanup shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    //==== Prepare For Rendering (VAO, VBO, EBO)
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(inds), inds, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void*)nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    LOG_INFO("OpenGL", "All done!");
+}
+
+// =======================
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     LOG_TRACE("OpenGL", "Resizing to " << width << "x" << height);
     glViewport(0, 0, width, height);
 }
 
-void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+void keyboard_callback(GLFWwindow* window, int key, int scancode, int action,
+                       int mods) {
     LOG_TRACE("OpenGL", "Passing key callback through abstraction layer");
     Keyboard::callback(key, action);
 }
@@ -63,8 +163,13 @@ bool OpenGLRenderer::create() {
     }
 
     glViewport(0, 0, m_Width, m_Height);
+
     LOG_INFO("OpenGL",
              "Initialised OpenGL viewport with " << m_Width << "x" << m_Height);
+
+
+
+    temp_shaders();
 
     return true;
 }
@@ -72,6 +177,10 @@ bool OpenGLRenderer::create() {
 void OpenGLRenderer::render() {
     glClearColor(0.2f, 0.2f, 0.6f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
     glfwSwapBuffers(m_Window);
     glfwPollEvents();
