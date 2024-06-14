1c8a091f6f3aadb078873bd06fc2d301a9b4077e
Xaaf <x.vandalen@outlook.com>
2024-06-14 14:59:41 +0200

    Added setters for shader attributes

--- engine/include/Colossus/Render/Arch/OpenGL/Shader.h
+++ engine/include/Colossus/Render/Arch/OpenGL/Shader.h
@@ -13,7 +13,12 @@ class Shader {
         ~Shader();
 
         void create(const char* vertexPath, const char* fragmentPath);
+
         void use() const;
+
+        void setBool(const std::string& name, bool value) const;
+        void setInt(const std::string& name, int value) const;
+        void setFloat(const std::string& name, float value) const;
 
     private:
         void checkStatusErrors(unsigned int object, const char* type) const;
--- engine/src/Colossus/Render/Arch/OpenGL/Shader.cpp
+++ engine/src/Colossus/Render/Arch/OpenGL/Shader.cpp
@@ -108,3 +108,15 @@ void Shader::checkStatusErrors(unsigned int object, const char *type) const {
         }
     }
 }
+
+void Shader::setBool(const std::string &name, bool value) const {
+    glUniform1i(glGetUniformLocation(programId, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string &name, int value) const {
+    glUniform1i(glGetUniformLocation(programId, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string &name, float value) const {
+    glUniform1f(glGetUniformLocation(programId, name.c_str()), value);
+}
